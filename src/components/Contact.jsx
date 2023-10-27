664ef7fe7533151f1a988a02477eef42a23d1ca3
MahimaPathak023 <pathakmahima57@gmail.com>
2023-10-27 23:01:11 +0530

    Final Changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import { EarthCanvas } from "./canvas";
 import  SectionWrapper  from "../hoc/SectionWrapper";
 import { slideIn } from "../utils/motion";
 
+//JmyEREMWBLuewbLXh
+//template_yfg0egp
+//service_kzc3hpe
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -33,16 +37,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_kzc3hpe',
+        'template_yfg0egp',
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Mahima Pathak",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "pathakmahima57@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'JmyEREMWBLuewbLXh'
       )
       .then(
         () => {
@@ -59,7 +63,7 @@ const Contact = () => {
           setLoading(false);
           console.error(error);
 
-          alert("Ahh, something went wrong. Please try again.");
+          alert("Something went wrong. Please try again.");
         }
       );
   };
